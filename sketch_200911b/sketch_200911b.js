f13195ae3a15750a5c438170979fac303da1d7f5
sr3874 <70740503+sr3874@users.noreply.github.com>
2020-09-18 13:30:42 +0900

    10804

---
+++ sketch_200911b/sketch_200911b.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(500,500);
+  
+  fill(255,20,20);
+  elipse(340,230,200,200);
+  fill(0,255,125);
+  elipse(100,100,20,20);
+  fill(125,0,255);
+  elipse(400,400,50,50);
+  fill(0,255,125);
+  elipse(300,300,25,25);
+  fill(255,0,125);
+  elipse(200,400,75,75);
+}
+
+
+function draw() {
+
+}
