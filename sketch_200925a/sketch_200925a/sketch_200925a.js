2c23f42d35537b3869463ef77946a0c4d9541002
sr3874 <70740503+sr3874@users.noreply.github.com>
2020-09-25 14:00:40 +0900

    sefsgsgfg

---
+++ sketch_200925a/sketch_200925a/sketch_200925a.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas (500,500);
+  for(let i=0; i<500; i+=100){
+    rect(i, 0, 100, 100);
+    rect(i, 100, 100, 100);
+    rect(i, 200, 100, 100);
+    rect(i, 300, 100, 100);
+    rect(i, 400, 100, 100);
+  }
+}
+
+
+function draw() {
+
+}
